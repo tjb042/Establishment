48157bfd23e2e57ad612582328b5feaa3a621ba2
tjb042 <Mb0w/kh62N3|3dxwxeEFu-3jq.JWW!,$>
2016-12-12 21:25:38 -0500

    Add additional comparison methods for string establisher

--- Establishment/StringEstablisher.cs
+++ Establishment/StringEstablisher.cs
@@ -24,6 +24,22 @@ namespace Establishment {
             return true;
         }
 
+        public bool IsEmpty(string value) {
+            if (value != string.Empty) {
+                return HandleFailure(new ArgumentException("string value must be empty"));
+            }
+
+            return true;
+        }
+
+        public bool IsNotEmpty(string value) {
+            if (value == string.Empty) {
+                return HandleFailure(new ArgumentException("string value must not be empty"));
+            }
+
+            return true;
+        }
+
     }
 
 }
