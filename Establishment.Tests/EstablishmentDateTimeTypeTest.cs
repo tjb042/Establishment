a29db6e246a80ff7991227d49b3e724c6ed9300d
tjb042 <Mb0w/kh62N3|3dxwxeEFu-3jq.JWW!,$>
2016-12-12 21:54:08 -0500

    Adding DateTimeEstablisher tests

---
+++ Establishment.Tests/EstablishmentDateTimeTypeTest.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Establishment.Tests {
+
+    [TestClass]
+    public class EstablishmentDateTimeTypeTest {
+
+        private DateTime GetUnixEpoch() {
+            return new DateTime(1970, 1, 1);
+        }
+
+        private DateTimeEstablisher CreateEstablisher() {
+            return new DateTimeEstablisher() {
+                ThrowExceptionOnEstablishmentFailure = true,
+            };
+        }
+
+        [TestMethod]
+        public void TestDateTime_IsAfterUnixEpoch() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsAfterUnixEpoch(DateTime.Now);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateTime_Negative_IsAfterUnixEpoch() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsAfterUnixEpoch(DateTime.MinValue);
+        }
+
+        [TestMethod]
+        public void TestDateTime_IsDefault() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsDefault(new DateTime());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateTime_Negative_IsDefault() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsDefault(DateTime.Now);
+        }
+
+        [TestMethod]
+        public void TestDateTime_IsEqual() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsEqual(new DateTime(1999, 1, 1), new DateTime(1999, 1, 1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateTime_Negative_IsEqual() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsEqual(DateTime.Now, DateTime.MinValue);
+        }
+
+        [TestMethod]
+        public void TestDateTime_IsGreaterThan() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsGreaterThan(DateTime.Now, DateTime.MinValue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateTime_Negative_IsGreaterThan() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsGreaterThan(DateTime.Now, DateTime.MaxValue);
+        }
+
+        [TestMethod]
+        public void TestDateTime_IsGreaterThanOrEqualTo() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsGreaterThanOrEqualTo(DateTime.Now, DateTime.MinValue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateTime_Negative_IsGreaterThanOrEqualTo() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsGreaterThanOrEqualTo(DateTime.Now, DateTime.MaxValue);
+        }
+
+        [TestMethod]
+        public void TestDateTime_IsLessThan() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsLessThan(DateTime.Now, DateTime.MaxValue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateTime_Negative_IsLessThan() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsLessThan(DateTime.Now, DateTime.MinValue);
+        }
+
+        [TestMethod]
+        public void TestDateTime_IsLessThanOrEqualTo() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsLessThanOrEqualTo(DateTime.Now, DateTime.MaxValue);
+        }
+    
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateTime_Negative_IsLessThanOrEqualTo() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsLessThanOrEqualTo(DateTime.Now, DateTime.MinValue);
+        }
+
+        [TestMethod]
+        public void TestDateTime_IsMaxDate() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsMaxDate(DateTime.MaxValue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateTime_Negative_IsMaxDate() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsMaxDate(DateTime.Now);
+        }
+
+        [TestMethod]
+        public void TestDateTime_IsMinDate() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsMinDate(DateTime.MinValue);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateTime_Negative_IsMinDate() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsMinDate(DateTime.Now);
+        }
+
+        [TestMethod]
+        public void TestDateTime_IsUnixEpoch() {
+            var establisher = CreateEstablisher();
+
+            establisher.IsUnixEpoch(GetUnixEpoch());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateTime_Negative_IsUnixEpoch() {
+            var establisher = CreateEstablisher();
+            var value = GetUnixEpoch().AddSeconds(1000);
+
+            establisher.IsUnixEpoch(value);
+        }
+
+    }
+
+}
