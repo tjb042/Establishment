df43c4533057a613949822ca1267568ce8d9e371
tjb042 <Mb0w/kh62N3|3dxwxeEFu-3jq.JWW!,$>
2016-12-18 19:17:33 -0500

    Adding new boolean positive and negative unit tests

---
+++ Establishment.Tests/BooleanEstablisherTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Establishment.Tests {
+
+    [TestClass]
+    public class BooleanEstablisherTests {
+    
+        [TestMethod]
+        public void Bool_IsTrue() {
+            var establisher = Establish.For(true);
+
+            establisher.IsTrue();
+        }
+
+        [TestMethod]
+        public void Bool_IsFalse() {
+            var establisher = Establish.For(false);
+
+            establisher.IsFalse();
+        }
+
+    }
+
+}
---
+++ Establishment.Tests/NegativeBooleanEstablisherTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Establishment.Tests {
+
+    [TestClass]
+    public class NegativeBooleanEstablisherTests {
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Negative_Bool_IsTrue() {
+            var establisher = Establish.For(false);
+
+            establisher.IsTrue();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Negative_Bool_IsFalse() {
+            var establisher = Establish.For(true);
+
+            establisher.IsFalse();
+        }
+
+    }
+
+}
