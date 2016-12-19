3bb00f36886b821ca0c9e0af8ac92da9dcf1c380
tjb042 <Mb0w/kh62N3|3dxwxeEFu-3jq.JWW!,$>
2016-12-18 20:31:11 -0500

    Resolving documentation and invalid parameters in BaseClassEstablisher

--- Establishment/BaseClassEstablisher.cs
+++ Establishment/BaseClassEstablisher.cs
@@ -15,6 +15,7 @@ namespace Establishment {
         /// <summary>
         /// Initializes a new instance of <see cref="BaseClassEstablisher"/>
         /// </summary>
+        /// <param name="baseline">The default value to load into this instance. Used for comparison tests</param>
         public BaseClassEstablisher(TType baseline) : base(baseline) {
             
         }
@@ -22,10 +23,9 @@ namespace Establishment {
         /// <summary>
         /// Establishes whether or not <paramref name="value"/> is <c>null</c>
         /// </summary>
-        /// <param name="value">The value to evaluate</param>
         /// <returns><c>true</c> if <paramref name="value"/> is null; otherwise, <c>false</c></returns>
-        public virtual BaseClassEstablisher<TType> IsNull(TType value) {
-            if (value != null) {
+        public virtual BaseClassEstablisher<TType> IsNull() {
+            if (Value != null) {
                 HandleFailure(new ArgumentException(GenericType.Name + " value must be null"));
             }
 
@@ -35,10 +35,9 @@ namespace Establishment {
         /// <summary>
         /// Establishes whether or not <paramref name="value"/> is not <c>null</c>
         /// </summary>
-        /// <param name="value">The value to evaluate</param>
         /// <returns><c>true</c> if <paramref name="value"/> is not null; otherwise, <c>false</c></returns>
-        public virtual BaseClassEstablisher<TType> IsNotNull(TType value) {
-            if (value == null) {
+        public virtual BaseClassEstablisher<TType> IsNotNull() {
+            if (Value == null) {
                 HandleFailure(new ArgumentException(GenericType.Name + " value cannot be null"));
             }
 
@@ -48,11 +47,10 @@ namespace Establishment {
         /// <summary>
         /// Establishes whether or not <paramref name="value"/> is equal to <paramref name="constraint"/>
         /// </summary>
-        /// <param name="value">The inital value to compare</param>
         /// <param name="constraint">The target value for comparison</param>
         /// <returns><c>true</c> if <paramref name="value"/> equals <paramref name="constraint"/>; otherwise, <c>false</c></returns>
-        public virtual BaseClassEstablisher<TType> IsEqual(TType value, TType constraint) {
-            if (!DefaultComparer.Equals(value, constraint)) {
+        public virtual BaseClassEstablisher<TType> IsEqual(TType constraint) {
+            if (!DefaultComparer.Equals(Value, constraint)) {
                 HandleFailure(new ArgumentException(GenericType.Name + " value must equal constraint"));
             }
 
@@ -62,11 +60,10 @@ namespace Establishment {
         /// <summary>
         /// Establishes whether or not <paramref name="value"/> is not equal to <paramref name="constraint"/>
         /// </summary>
-        /// <param name="value">The inital value to compare</param>
         /// <param name="constraint">The target value for comparison</param>
         /// <returns><c>true</c> if <paramref name="value"/> does not equal <paramref name="constraint"/>; otherwise, <c>false</c></returns>
-        public virtual BaseClassEstablisher<TType> IsNotEqual(TType value, TType constraint) {
-            if (DefaultComparer.Equals(value, constraint)) {
+        public virtual BaseClassEstablisher<TType> IsNotEqual(TType constraint) {
+            if (DefaultComparer.Equals(Value, constraint)) {
                 HandleFailure(new ArgumentNullException(GenericType.Name + "value must not equal constraint"));
             }
 
