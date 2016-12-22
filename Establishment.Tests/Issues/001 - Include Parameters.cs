19a62e81d3794574b79c7e1eb91b5d73b40c2aba
tjb042 <Mb0w/kh62N3|3dxwxeEFu-3jq.JWW!,$>
2016-12-21 21:13:57 -0500

    Added unit tests for issue #1

---
+++ Establishment.Tests/Issues/001 - Include Parameters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Establishment.Tests.Issues {
+
+    [TestClass]
+    public class Issue001 {
+        
+        [TestMethod]
+        public void IncludeParameterName() {
+            var parameterName = "isEnabled";
+            var establish = Establish.For(true, parameterName);
+
+            try {
+                establish.IsFalse();
+                Assert.Fail();
+            }
+            catch (ArgumentException ae) {
+                Assert.AreEqual(ae.ParamName, parameterName);
+            }
+        }
+
+        [TestMethod]
+        public void DoNotIncludeParameterName() {
+            var establish = Establish.For(true);
+
+            try {
+                establish.IsFalse();
+                Assert.Fail();
+            }
+            catch (ArgumentException ae) {
+                Assert.IsTrue(string.IsNullOrEmpty(ae.ParamName));
+            }
+        }
+
+    }
+
+}
