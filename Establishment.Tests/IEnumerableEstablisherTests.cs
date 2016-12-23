9837bd0868289cd291a1e8ea49f5d31034dc92bf
tjb042 <Mb0w/kh62N3|3dxwxeEFu-3jq.JWW!,$>
2016-12-23 17:52:42 -0500

    Adding unit tests for IEnumerableEstablisher per issue #7

---
+++ Establishment.Tests/IEnumerableEstablisherTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Establishment.Tests {
+
+    [TestClass]
+    public class IEnumerableEstablisherTests {
+
+        [TestMethod]
+        public void IEnumerableEstablisher_HasElements() {
+            var establisher = Establish.For(new string[] { "hello world" });
+
+            establisher.HasElements();
+        }
+
+        [TestMethod]
+        public void IEnumerableEstablisher_HasNoElements() {
+            var establisher = Establish.For(new List<int>());
+
+            establisher.HasNoElements();
+        }
+
+        [TestMethod]
+        public void IEnumerableEstablisher_HasExactLength() {
+            var establisher = Establish.For(new int[] { 1, 2, 3 });
+
+            establisher.HasExactLength(3);
+        }
+
+        [TestMethod]
+        public void IEnumerableEstablisher_HasMaximumLength() {
+            var establisher = Establish.For(new Dictionary<string, string>() { { "key", "value" } });
+
+            establisher.HasMaximumLength(2);
+            establisher.HasMaximumLength(1);
+        }
+
+        [TestMethod]
+        public void IEnumerableEstablisher_HasMinimumLength() {
+            var establisher = Establish.For(new long[] { 1L, 5L, 1000L });
+
+            establisher.HasMinimumLength(0);
+            establisher.HasMinimumLength(3);
+        }
+
+    }
+
+}
