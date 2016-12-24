b78d070a7d5e4e0cdbaa5deb044397814348472a
tjb042 <Mb0w/kh62N3|3dxwxeEFu-3jq.JWW!,$>
2016-12-23 21:25:24 -0500

    Add custom exception per issue #11

---
+++ Establishment/Exceptions/EstablishmentException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Establishment.Exceptions {
+
+    [Serializable]
+    public class EstablishmentException : ArgumentException {
+
+        public EstablishmentException() { }
+
+        public EstablishmentException(string message) : base(message) { }
+
+        public EstablishmentException(string message, Exception innerException) : base(message, innerException) { }
+
+        public EstablishmentException(string message, string paramName) : base(message, paramName) { }
+
+        public EstablishmentException(string message, string paramName, Exception innerException) : base(message, paramName, innerException) { }
+
+    }
+
+}
